ea1eb7aa61501e24c6b8e8ac0507a4b23bd3f80f
xiaoxingyu <2585931235@qq.com>
2023-11-12 21:14:02 +0800

    创建springboot文件夹

---
+++ learndemo/src/main/java/top/xiaodashuai/spring2/learndemo/LearndemoApplication.java
@@ -0,0 +1,13 @@
+package top.xiaodashuai.spring2.learndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearndemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearndemoApplication.class, args);
+	}
+
+}
---
+++ learndemo/src/test/java/top/xiaodashuai/spring2/learndemo/LearndemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xiaodashuai.spring2.learndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearndemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
